9d5f0ca98b61a689d7b81c5d70a63eefe4c67d79
Moocchi <muhammadjarez@gmail.com>
2025-07-11 21:31:12 +0700

    initial commit

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,31 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        if (User::count() == 0) {
-            // Ensure roles exist
-            Role::firstOrCreate(['name' => 'super_admin']);
-            Role::firstOrCreate(['name'=> 'petugas']);
-
-            $superAdmin = User::factory()->create([
-                'name' => 'Super Admin',
-                'email' => 'admin@admin.com',
-                'password' => bcrypt('password'),
-            ]);
-            $superAdmin->assignRole('super_admin');
-            
-            $petugas = User::factory()->create([
-                'name' => 'Petugas',
-                'email' => 'petugas@warehouse.com',
-                'password' => bcrypt('password'),
-            ]);
-            $petugas->assignRole('petugas');
-        }
-
-
-        $this->call([
-            GudangSeeder::class,
-            ThemeSeeder::class,
-            RolePermissionSeeder::class,
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
         ]);
+
+        $user->assignRole('super_admin');
     }
 }
